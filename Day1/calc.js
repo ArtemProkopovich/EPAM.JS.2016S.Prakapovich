71f684fb918d58866cca1e1aa30b7c81388a724e
Артем Прокопович < >
2016-08-10 15:18:14 +0300

    day 1 task implemented

---
+++ Day1/calc.js
@@ -0,0 +1,10 @@
+for (var i = 0; i < data.length; i++) {
+    if (data[i] != null) {
+        if (data[i] == 0)
+            data[i] += 10;
+        else if (data[i] > 100)
+            data[i] -= 100;
+        else if (data[i] < 100)
+            data[i] += 100;
+    }
+}
